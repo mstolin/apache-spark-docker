076d9e51cf7966a0649afef1f94014bd73bca61a
Marcel Stolin <marcelstolin@gmail.com>
2020-10-12 13:37:04 +0200

    Fixed build script paths

--- build-images.sh
+++ build-images.sh
@@ -4,10 +4,10 @@ SPARK_VERSION=$1
 HADOOP_VERSION=$2
 
 echo '# Building spark-base'
-docker build -t spark-base ./docker/spark-base/ --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION
+docker build -t spark-base ./images/spark-base/ --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION
 
 echo '# Building spark-master'
-docker build -t spark-master ./docker/spark-master/
+docker build -t spark-master ./images/spark-master/
 
 echo '# Building spark-worker'
-docker build -t spark-worker ./docker/spark-worker/
+docker build -t spark-worker ./images/spark-worker/
