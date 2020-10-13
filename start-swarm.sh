28094df6afa38d3fdf8b07c749ab5aa2c7edbbba
Marcel Stolin <marcelstolin@gmail.com>
2020-10-13 11:03:49 +0200

    Created one single stack called spark_cluster

--- start-swarm.sh
+++ start-swarm.sh
@@ -5,8 +5,5 @@ PWD=$(pwd)
 echo '# Initializing swarm'
 docker swarm init
 
-echo '\n# Deploying computing stack'
-docker stack deploy -c $PWD/stacks/computing.yml computing
-
-echo '\n# eploying monitoring stack'
-docker stack deploy -c $PWD/stacks/monitoring.yml monitoring
+echo '\n# Deploying spark-cluster stack'
+docker stack deploy -c $PWD/stacks/spark-cluster.yml spark_cluster
