0d34dc334344aaef9bc57af4fb87e84a75c54978
Marcel Stolin <marcelstolin@gmail.com>
2020-10-15 17:18:11 +0200

    FIxed submit script for now

--- images/spark-submit/submit.sh
+++ images/spark-submit/submit.sh
@@ -2,23 +2,28 @@
 
 echo "##### Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URI}"
 
-if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
-    echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URI}"
-    echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
-    ${SPARK_HOME}/bin/spark-submit \
-        --class ${SPARK_APPLICATION_MAIN_CLASS} \
-        --master ${SPARK_MASTER_URI} \
-        ${SPARK_SUBMIT_ARGS} \
-        ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
-else
-    if [ -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
-        echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URI}"
-        echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
-        PYSPARK_PYTHON=python3 ${SPARK_HOME}/bin/spark-submit \
+PYSPARK_PYTHON=python3 ${SPARK_HOME}/bin/spark-submit \
             --master ${SPARK_MASTER_URI} \
             ${SPARK_SUBMIT_ARGS} \
             ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
-    else
-        echo "Not recognized application."
-    fi
-fi
+
+#if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
+#    echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URI}"
+#    echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
+#    ${SPARK_HOME}/bin/spark-submit \
+#        --class ${SPARK_APPLICATION_MAIN_CLASS} \
+#        --master ${SPARK_MASTER_URI} \
+#        ${SPARK_SUBMIT_ARGS} \
+#        ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
+#else
+#    if [ -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
+#        echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URI}"
+#        echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
+#        PYSPARK_PYTHON=python3 ${SPARK_HOME}/bin/spark-submit \
+#            --master ${SPARK_MASTER_URI} \
+#            ${SPARK_SUBMIT_ARGS} \
+#            ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
+#    else
+#        echo "Not recognized application."
+#    fi
+#fi
