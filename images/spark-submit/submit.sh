c33da951ec46c51730c3fb78e4b1302e4140b8d7
Marcel Stolin <marcelstolin@gmail.com>
2020-10-16 11:55:00 +0200

    Updated submit.sh

--- images/spark-submit/submit.sh
+++ images/spark-submit/submit.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-echo "#### PWD"
-pwd
-
-echo "#### WHICH SUBMIT"
-which submit
-
-echo "##### Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URI}"
-echo "##### Passing arguments ${SPARK_APPLICATION_ARGS}"
-
-PYSPARK_PYTHON=python3 ${SPARK_HOME}/bin/spark-submit \
-            --master ${SPARK_MASTER_URI} \
-            ${SPARK_SUBMIT_ARGS} \
-            ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
+${SPARK_HOME}/bin/spark-submit \
+    --master ${SPARK_MASTER_URI} \
+    ${SPARK_SUBMIT_ARGS} \
+    ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
 
 #if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
 #    echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URI}"
